1e2330805ec52c51edec085541657dc6b25cb285
muhammadammar2 <uExistentialist@proton.me>
2024-07-02 15:18:32 +0500

    Apparently backend is done including A*

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { H3Service } from './h3/h3.service';
+import { H3Controller } from './h3/h3.controller';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [H3Controller],
+  providers: [H3Service],
 })
 export class AppModule {}
